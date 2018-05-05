92b7be833be470f078fad7dd12258330e363026d
Mohammad Inzamam Malik <malikasinger1@gmail.com>
2018-05-05 16:08:47 +0500

    first intent

--- functions/lib/index.js
+++ functions/lib/index.js
@@ -1,31 +1,24 @@
-import * as functions from 'firebase-functions';
-
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const functions = require("firebase-functions");
 const { WebhookClient } = require('dialogflow-fulfillment');
-
-
-
-export const dialogflowFulfillment = functions.https.onRequest((request, response) => {
-
+exports.dialogflowFulfillment = functions.https.onRequest((request, response) => {
     const _agent = new WebhookClient({ request: request, response: response });
     console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
     console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
-
-
     function welcome(agent) {
         agent.add(`Welcome to my agent!`);
     }
-
     function fallback(agent) {
         agent.add(`I didn't understand`);
         agent.add(`I'm sorry, can you try again?`);
     }
-
     // Run the proper function handler based on the matched Dialogflow intent name
     let intentMap = new Map();
     intentMap.set('input.welcome', welcome);
     intentMap.set('input.unknown', fallback);
     // intentMap.set('your intent name here', yourFunctionHandler);
     // intentMap.set('your intent name here', googleAssistantHandler);
-    _agent.handler(intentMap);
-
+    _agent.handleRequest(intentMap);
 });
+//# sourceMappingURL=index.js.map
