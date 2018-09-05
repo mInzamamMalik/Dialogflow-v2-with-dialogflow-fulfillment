14ac585a48395eba7245232868d5a1710485a41a
Mohammad Inzamam Malik <malikasinger1@gmail.com>
2018-09-05 22:33:16 +0500

    added v2 entity helper function

---
+++ functions/src/helperfunctions/userEntityv2.ts
@@ -0,0 +1,103 @@
+import * as functions from 'firebase-functions'
+import * as request from 'request'
+
+export interface entityEntry {
+    "value": string,
+    "synonyms": string[]
+}
+
+export class userEntity {
+
+
+    static makeUserEntity = function (
+        accessToken: "string",
+        session: RegExp = /^projects\/[^\/]+\/agent\/sessions\/[^\/]+$/,
+        entityName: string,
+        entries: entityEntry[]
+    ) {
+
+        return new Promise((resolve, reject) => {
+            // adding all organizations in apiai userEntity
+            request.post({
+                url: `https://dialogflow.googleapis.com/v2/${session}/entityTypes/`,
+                headers: {
+                    "Authorization": "Bearer " + accessToken
+                },
+                json: {
+                    "name": `${session}/entityTypes/${entityName}`,
+                    "entityOverrideMode": "ENTITY_OVERRIDE_MODE_OVERRIDE",
+                    "entries": entries
+                }
+            }, function (error: any, response: any, body: any) {
+
+                console.log(`on ${accessToken} making entity ${entityName} on session ${session} response: `, response.body);
+                //checking if response was success
+                if (!error && response.statusCode === 200) {
+
+                    resolve(response.body);
+
+                } else {
+                    // console.log("error in making user /entity: ", response.statusCode, error);
+                    reject(error)
+                }
+            })
+        })//promise end
+    }//makeUserEntity end
+
+
+    static makeUserEntityWithArray = function (sessionId: string, entityName: string, entries: string[], isDry = false) {
+        return new Promise((resolve, reject) => {
+
+            const newentityEntry: entityEntry[] = [];
+            entries.map((name, index) => {
+
+                let value = name; //temp variable
+                let synonyms = [name]; //temp variable
+
+                if (!isDry) {
+                    switch (index) {
+                        case 0:
+                            synonyms.push("1")
+                            synonyms.push("1st")
+                            synonyms.push("first")
+                            synonyms.push("first option")
+                            synonyms.push("one")
+                            synonyms.push("option one")
+                            break;
+                        case 1:
+                            synonyms.push("2")
+                            synonyms.push("2nd")
+                            synonyms.push("2nd option")
+                            synonyms.push("second")
+                            synonyms.push("second option")
+                            synonyms.push("two")
+                            synonyms.push("option two")
+                            break;
+                        case 2:
+                            synonyms.push("3")
+                            synonyms.push("3rd")
+                            synonyms.push("3rd option")
+                            synonyms.push("third")
+                            synonyms.push("third option")
+                            synonyms.push("three")
+                            synonyms.push("option three")
+                            break;
+                        default:
+                            synonyms.push("" + (index + 1))
+                            synonyms.push("" + (index + 1) + "th")
+                            synonyms.push("" + (index + 1) + "th option")
+                            break;
+                    }
+                }
+                newentityEntry.push({
+                    value: value, // value will look like: "geo fence group"
+                    synonyms: synonyms // synonyms looks like: ["geo fence group", "1", "1st", "first"]
+                })
+            })
+            this.makeUserEntity(sessionId, entityName, newentityEntry).then(response => {
+                resolve(response)
+            })
+        })//promise end
+    }
+
+}
